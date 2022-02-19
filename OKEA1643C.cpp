e3c15d6c71c053ae4d07ee8f196786c55ece5d2b
Nguyễn Lê Đông Mạnh <98057363+nguyenledongmanh@users.noreply.github.com>
2022-02-19 22:13:56 +0700

    Bài 14

---
+++ OKEA1643C.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int testcase;
+    cin >> testcase;
+    while(testcase--) {
+        int n, k;
+        cin >> n >> k;
+        if (k == 1) {
+            cout << "YES" << endl;
+            for (int i = 1; i <= n; i++)
+                cout << i << endl;
+        }
+        else if (k == 2 && n % 2 == 0)
+        {
+            cout << "YES" << endl;
+            int flag = 1;
+            while (n)
+            {
+                cout << flag << " " << flag + 2 << endl;
+                cout << flag + 1 << " " << flag + 3 << endl;
+                flag = flag + 4;
+                n -= 2;
+            }
+        }
+        else {
+            if ((n * k) % 2 != 0 || n % 2 != 0)
+                cout << "NO" << endl;
+            else {
+                cout << "YES" << endl;
+                int flag = 1;
+                while(n > 0) {
+                    for (int i = 0; i < k; i++) {
+                        cout << flag + 2 * i << " ";
+                    }
+                    cout << endl;
+                    flag += 1;
+                    for (int i = 0; i < k; i++) {
+                        cout << flag + 2 * i << " ";
+                    }
+                    cout << endl;
+                    flag += 2 * (k - 1) + 1;
+                    n -= 2;
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
